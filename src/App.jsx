a3d99a6dca4f7687ff1c4f7a3c0616fca13a65e9
davidmunozvi <davidmunozvicode@gmail.com>
2023-09-08 14:01:24 +0200

    feat: create cities Api repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { createApiWeatherRepository } from '@/modules/weatherReports/infrastructure/ApiWeatherRepository';
+import { createApiCitiesRepository } from '@/modules/cities/infrastructure/ApiCitiesRepository';
 import { httpClient } from '@/modules/shared/infrastructure/HttpClient';
 import { useEffect, useState } from 'react';
 const client = httpClient;
 const weatherRepository = createApiWeatherRepository(client);
+const citiesRepository = createApiCitiesRepository(client);
 
 function App() {
 	const [weather, setWeather] = useState({});
-
+	const [cities, setCities] = useState([]);
 	useEffect(() => {
 		weatherRepository
 			.getWeatherByCoordinates({
@@ -15,10 +17,20 @@ function App() {
 			})
 			.then(data => setWeather(data));
 	}, []);
+	useEffect(() => {
+		citiesRepository.getAll('Madrid').then(data => setCities(data));
+	}, []);
 
 	return (
 		<>
 			<div>{weather?.timezone} hor</div>
+			<div>
+				{cities.map(city => (
+					<div key={city.id}>
+						{city.name} {city.country}
+					</div>
+				))}
+			</div>
 		</>
 	);
 }
