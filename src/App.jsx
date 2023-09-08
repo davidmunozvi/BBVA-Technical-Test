06f9bf198093192965d935dee119e91d4984002d
davidmunozvi <davidmunozvicode@gmail.com>
2023-09-08 13:59:41 +0200

    feat: create weather api repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { createApiWeatherRepository } from '@/modules/weatherReports/infrastructure/ApiWeatherRepository';
+import { httpClient } from '@/modules/shared/infrastructure/HttpClient';
+import { useEffect, useState } from 'react';
+const client = httpClient;
+const weatherRepository = createApiWeatherRepository(client);
+
 function App() {
+	const [weather, setWeather] = useState({});
+
+	useEffect(() => {
+		weatherRepository
+			.getWeatherByCoordinates({
+				latitude: 40.4165,
+				longitude: -3.70256,
+			})
+			.then(data => setWeather(data));
+	}, []);
+
 	return (
 		<>
-			<div>Weather app</div>
+			<div>{weather?.timezone} hor</div>
 		</>
 	);
 }
